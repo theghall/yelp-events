45da33d578d4cb0795cc15d9b7360392034bbb09
Gary Hall <guategary@gmail.com>
2018-08-09 15:42:29 -0400

    Add authentication

--- test/integration/event_flow_test.rb
+++ test/integration/event_flow_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class EventFlowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:john)
+  end
+
+  test "it should display login page" do
+    get root_url
+    follow_redirect!
+    assert_template "devise/sessions/new"
+  end
+
+  test "it should display event search page" do
+    sign_in @user
+    get root_url
+    assert_template "static/home"
+  end
 end
