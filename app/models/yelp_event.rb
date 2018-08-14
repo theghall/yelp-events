5683cbe5d060c7ee72fb80b10074518c2585e1f0
Gary Hall <guategary@gmail.com>
2018-08-13 21:07:54 -0400

    Add scopes to YelpEvent for attending yes and maybe

--- app/models/yelp_event.rb
+++ app/models/yelp_event.rb
@@ -1,4 +1,6 @@
 class YelpEvent < ApplicationRecord
   belongs_to :user
   default_scope -> { order(start_date: :desc) }
+  scope :attending_yes, -> { where(attending: 'yes') }
+  scope :attending_maybe, -> { where(attending: 'maybe') }
 end
