34dd4a3d095e47fdb78fcbdedf90ae6d367dc61c
Gary Hall <guategary@gmail.com>
2018-08-20 15:47:12 -0400

    fix-sw-heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.configure do
-  config.assets.precompile += %w[/serviceworkers/event-worker.js]
+  config.assets.precompile += %w[serviceworkers/event-worker.js]
 end
