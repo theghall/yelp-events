5da5fb539ac6294eab2c3c32f52b2a2ec8577a1e
Gary Hall <guategary@gmail.com>
2018-08-23 13:54:50 -0400

    Add manifest.json

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.serviceworker.routes.draw do
     match "/event-worker.js" => 'serviceworkers/event-worker.js'
+    match "/manifest.json" => 'serviceworkers/manifest.json'
   end
 end
