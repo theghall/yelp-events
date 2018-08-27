665eba2c34ed4581b4748af746be47366f80dae1
Gary Hall <guategary@gmail.com>
2018-08-27 02:04:46 -0400

    Fix bug where sw tried to create offline page when not logged in yet

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,7 +1,27 @@
+function isLoggedIn() {
+  const signinDiv = document.getElementById('login');
+
+  if (signinDiv === null) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 if (navigator.serviceWorker) {
+  // TODO: refactor to use async/await
   navigator.serviceWorker.register('/event-worker.js', { scope: '/'})
     .then(function(reg) {
       console.log('event-worker registered');
+      if (navigator.serviceWorker.controller) {
+        // Recreate the offline page when user logs in
+        // TODO: restrict to root page
+        window.addEventListener('load', function() {
+          if (isLoggedIn()) {
+            navigator.serviceWorker.controller.postMessage('create offline page');
+          }
+        });
+      }
     })
     .catch(function(err) {
       console.log('event-worker registration failed: ', err);
